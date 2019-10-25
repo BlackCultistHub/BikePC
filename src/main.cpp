8f1bd9bf38b9084b0450fde8259536389b42908c
BlackCultistHub <tema-vil@bk.ru>
2019-10-25 19:47:47 +0300

    Added summ

--- src/main.cpp
+++ src/main.cpp
@@ -5,8 +5,15 @@ void setup()
   Serial.begin (9600);
 }
 
+void init()
+{
+  int a = 10,
+      b = 5;
+}
+
 void loop() 
 {
-  Serial.print("Hello world. This is test\n");
+  Serial.print("Hello world. This is test:\n\t");
+  Serial.print("a = 10, b = 5");
   delay(1000);
 }
