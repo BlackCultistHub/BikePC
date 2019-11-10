cd0b7ceed7723db02194c836c68efff2ede74ccd
BlackCultist <tema-vil@bk.ru>
2019-11-10 05:10:22 +0300

    add wifi

--- src/main.cpp
+++ src/main.cpp
@@ -1,59 +1,141 @@
+#include <Arduino.h>
 #include <Adafruit_GFX.h>      // include Adafruit graphics library
 #include <Adafruit_ST7735.h>   // include Adafruit ST7735 TFT library
+#include <ESP8266WiFi.h>
+//#include <WiFiClient.h>
+//#include <ESP8266WebServer.h>
  
-// ST7735 TFT module connections
+//***********[ ST7735 TFT module connections ]***********
 #define TFT_RST   D4     // TFT RST pin is connected to NodeMCU pin D4 (GPIO2)
 #define TFT_CS    D3     // TFT CS  pin is connected to NodeMCU pin D4 (GPIO0)
 #define TFT_DC    D2     // TFT DC  pin is connected to NodeMCU pin D4 (GPIO4)
 // initialize ST7735 TFT library with hardware SPI module
 // SCK (CLK) ---> NodeMCU pin D5 (GPIO14)
 // MOSI(DIN) ---> NodeMCU pin D7 (GPIO13)
-Adafruit_ST7735 tft = Adafruit_ST7735(TFT_CS, TFT_DC, TFT_RST);
 
-float p = 3.1415926;
- 
-//FUNCS BEGIN
-/*void testlines(uint16_t color) {
+//***********[ Predefined Values ]***********
+#define WIFI_SSID "InterZet610 (5GHz)"
+#define WIFI_PASSWORD "0987654321000"
+
+//***********[ Object Inits ]***********
+Adafruit_ST7735 tft(TFT_CS, TFT_DC, TFT_RST); //tft screen object
+WiFiServer server(80);
+
+//***********[ Function Prototypes ]***********
+String prepareHtmlPage();
+void refresh(Adafruit_ST7735*);
+
+//***********[ Inits and One-Time Run ]***********
+void setup(void) 
+{
+  tft.initR(INITR_BLACKTAB);   // initialize a ST7735S chip, black tab
   tft.fillScreen(ST7735_BLACK);
-  for (int16_t x=0; x < tft.width(); x+=6) {
-    tft.drawLine(0, 0, x, tft.height()-1, color);
-  }
-  for (int16_t y=0; y < tft.height(); y+=6) {
-    tft.drawLine(0, 0, tft.width()-1, y, color);
-  }
- 
-  tft.fillScreen(ST7735_BLACK);
-  for (int16_t x=0; x < tft.width(); x+=6) {
-    tft.drawLine(tft.width()-1, 0, x, tft.height()-1, color);
-  }
-  for (int16_t y=0; y < tft.height(); y+=6) {
-    tft.drawLine(tft.width()-1, 0, 0, y, color);
-  }
- 
-  tft.fillScreen(ST7735_BLACK);
-  for (int16_t x=0; x < tft.width(); x+=6) {
-    tft.drawLine(0, tft.height()-1, x, 0, color);
-  }
-  for (int16_t y=0; y < tft.height(); y+=6) {
-    tft.drawLine(0, tft.height()-1, tft.width()-1, y, color);
-  }
- 
-  tft.fillScreen(ST7735_BLACK);
-  for (int16_t x=0; x < tft.width(); x+=6) {
-    tft.drawLine(tft.width()-1, tft.height()-1, x, 0, color);
-  }
-  for (int16_t y=0; y < tft.height(); y+=6) {
-    tft.drawLine(tft.width()-1, tft.height()-1, 0, y, color);
-  }
+  delay(500);
+  tft.printf("Connecting to\n%s", WIFI_SSID);
+  WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
+  delay(3000);
+  /*while (WiFi.status() != WL_CONNECTED)
+  {
+    delay(500);
+    tft.print(".");
+  }*/
+  tft.println("Сonnected.");
+  server.begin();
+  tft.printf("Web server started, open %s in a web browser\n", WiFi.localIP().toString().c_str());
+  /* tft.drawRect(0,0,128,25,ST7735_WHITE);
+  tft.drawRect(0,25,128,80,ST7735_WHITE);
+  tft.drawRect(0,105,128,55,ST7735_WHITE);
+  tft.setTextColor(ST7735_WHITE);
+  tft.setCursor(5, 5);
+  tft.setTextSize(1);
+  tft.println("12:00");
+  tft.setTextSize(0);
+  tft.setCursor(5, 15);
+  tft.println("01.10.19");
+  tft.setCursor(105,5);
+  tft.print("90%");
+  tft.setTextSize(5);
+  tft.setCursor(20,50);
+  tft.print("19");
+  tft.setTextSize(1);
+  tft.println(" km/h");
+  tft.setCursor(5, 110);
+  tft.setTextSize(2);
+  tft.print("o 12'000");
+  tft.setTextSize(1);
+  tft.println(" rpm");
+  tft.setTextSize(2);
+  tft.setCursor(5, 140);
+  tft.print("H 90");
+  tft.setTextSize(1);
+  tft.println(" b/min"); */
 }
- 
+
+//***********[ Inf Loop ]***********
+void loop(void) 
+{
+  WiFiClient client = server.available();
+  // wait for a client (web browser) to connect
+  if (client)
+  {
+    tft.println("\n[Client connected]");
+    while (client.connected())
+    {
+      // read line by line what the client (web browser) is requesting
+      if (client.available())
+      {
+        String line = client.readStringUntil('\r');
+        tft.print(line);
+        // wait for end of client's request, that is marked with an empty line
+        if (line.length() == 1 && line[0] == '\n')
+        {
+          client.println(prepareHtmlPage());
+          break;
+        }
+      }
+    }
+    delay(1); // give the web browser time to receive the data
+
+    // close the connection:
+    client.stop();
+    tft.println("[Client disonnected]");
+  }
+  refresh(&tft);
+}
+
+//***********[ Function Definitions ]***********
+String prepareHtmlPage()
+{
+  String htmlPage =
+     String("HTTP/1.1 200 OK\r\n") +
+            "Content-Type: text/html\r\n" +
+            "Connection: close\r\n" +  // the connection will be closed after completion of the response
+            "Refresh: 5\r\n" +  // refresh the page automatically every 5 sec
+            "\r\n" +
+            "<!DOCTYPE HTML>" +
+            "<html>" +
+            "Random Value:  " + String(rand()%100+1) +
+            "</html>" +
+            "\r\n";
+  return htmlPage;
+}
+
+void refresh(Adafruit_ST7735* screen)
+{
+  screen->print("[REFRESH IN 3s]");
+  delay(3000);
+  screen->fillScreen(ST7735_BLACK);
+  screen->setCursor(0,0);
+}
+
+//EXAMPLE FUNCS BEGIN
+/*
 void testdrawtext(char *text, uint16_t color) {
   tft.setCursor(0, 0);
   tft.setTextColor(color);
   tft.setTextWrap(true);
   tft.print(text);
 }
- 
 void testfastlines(uint16_t color1, uint16_t color2) {
   tft.fillScreen(ST7735_BLACK);
   for (int16_t y=0; y < tft.height(); y+=5) {
@@ -63,77 +145,6 @@ void testfastlines(uint16_t color1, uint16_t color2) {
     tft.drawFastVLine(x, 0, tft.height(), color2);
   }
 }
- 
-void testdrawrects(uint16_t color) {
-  tft.fillScreen(ST7735_BLACK);
-  for (int16_t x=0; x < tft.width(); x+=6) {
-    tft.drawRect(tft.width()/2 -x/2, tft.height()/2 -x/2 , x, x, color);
-  }
-}
- 
-void testfillrects(uint16_t color1, uint16_t color2) {
-  tft.fillScreen(ST7735_BLACK);
-  for (int16_t x=tft.width()-1; x > 6; x-=6) {
-    tft.fillRect(tft.width()/2 -x/2, tft.height()/2 -x/2 , x, x, color1);
-    tft.drawRect(tft.width()/2 -x/2, tft.height()/2 -x/2 , x, x, color2);
-  }
-}
- 
-void testfillcircles(uint8_t radius, uint16_t color) {
-  for (int16_t x=radius; x < tft.width(); x+=radius*2) {
-    for (int16_t y=radius; y < tft.height(); y+=radius*2) {
-      tft.fillCircle(x, y, radius, color);
-    }
-  }
-}
- 
-void testdrawcircles(uint8_t radius, uint16_t color) {
-  for (int16_t x=0; x < tft.width()+radius; x+=radius*2) {
-    for (int16_t y=0; y < tft.height()+radius; y+=radius*2) {
-      tft.drawCircle(x, y, radius, color);
-    }
-  }
-}
- 
-void testtriangles() {
-  tft.fillScreen(ST7735_BLACK);
-  int color = 0xF800;
-  int t;
-  int w = tft.width()/2;
-  int x = tft.height()-1;
-  int y = 0;
-  int z = tft.width();
-  for(t = 0 ; t <= 15; t++) {
-    tft.drawTriangle(w, y, y, x, z, x, color);
-    x-=4;
-    y+=4;
-    z-=4;
-    color+=100;
-  }
-}
- 
-void testroundrects() {
-  tft.fillScreen(ST7735_BLACK);
-  int color = 100;
-  int i;
-  int t;
-  for(t = 0 ; t <= 4; t+=1) {
-    int x = 0;
-    int y = 0;
-    int w = tft.width()-2;
-    int h = tft.height()-2;
-    for(i = 0 ; i <= 16; i+=1) {
-      tft.drawRoundRect(x, y, w, h, 5, color);
-      x+=2;
-      y+=3;
-      w-=4;
-      h-=6;
-      color+=1100;
-    }
-    color+=100;
-  }
-}
- 
 void tftPrintTest() {
   tft.setTextWrap(false);
   tft.fillScreen(ST7735_BLACK);
@@ -172,125 +183,5 @@ void tftPrintTest() {
   tft.setTextColor(ST7735_WHITE);
   tft.print(" seconds.");
 }
- 
-void mediabuttons() {
-  // play
-  tft.fillScreen(ST7735_BLACK);
-  tft.fillRoundRect(25, 10, 78, 60, 8, ST7735_WHITE);
-  tft.fillTriangle(42, 20, 42, 60, 90, 40, ST7735_RED);
-  delay(500);
-  // pause
-  tft.fillRoundRect(25, 90, 78, 60, 8, ST7735_WHITE);
-  tft.fillRoundRect(39, 98, 20, 45, 5, ST7735_GREEN);
-  tft.fillRoundRect(69, 98, 20, 45, 5, ST7735_GREEN);
-  delay(500);
-  // play color
-  tft.fillTriangle(42, 20, 42, 60, 90, 40, ST7735_BLUE);
-  delay(50);
-  // pause color
-  tft.fillRoundRect(39, 98, 20, 45, 5, ST7735_RED);
-  tft.fillRoundRect(69, 98, 20, 45, 5, ST7735_RED);
-  // play color
-  tft.fillTriangle(42, 20, 42, 60, 90, 40, ST7735_GREEN);
-}*/
+*/
 //FUNCS END
-
-//char* concat(char*, char*);
-
-void setup() 
-{
-  tft.initR(INITR_BLACKTAB);   // initialize a ST7735S chip, black tab
- 
-  uint16_t time = millis();
-  tft.fillScreen(ST7735_BLACK);
-  time = millis() - time;
-  
-  delay(500);
- /*
-  //large block of text
-  tft.fillScreen(ST7735_BLACK);
-  testdrawtext("Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur adipiscing ante sed nibh tincidunt feugiat. Maecenas enim massa, fringilla sed malesuada et, malesuada sit amet turpis. Sed porttitor neque ut ante pretium vitae malesuada nunc bibendum. Nullam aliquet ultrices massa eu hendrerit. Ut sed nisi lorem. In vestibulum purus a tortor imperdiet posuere. ", ST7735_WHITE);
-  delay(1000);
- 
-  // tft print function!
-  tftPrintTest();
-  delay(4000);
- 
-  // a single pixel
-  tft.drawPixel(tft.width()/2, tft.height()/2, ST7735_GREEN);
-  delay(500);
- 
-  // line draw test
-  testlines(ST7735_YELLOW);
-  delay(500);
- 
-  // optimized lines
-  testfastlines(ST7735_RED, ST7735_BLUE);
-  delay(500);
- 
-  testdrawrects(ST7735_GREEN);
-  delay(500);
- 
-  testfillrects(ST7735_YELLOW, ST7735_MAGENTA);
-  delay(500);
- 
-  tft.fillScreen(ST7735_BLACK);
-  testfillcircles(10, ST7735_BLUE);
-  testdrawcircles(10, ST7735_WHITE);
-  delay(500);
- 
-  testroundrects();
-  delay(500);
- 
-  testtriangles();
-  delay(500);
- 
-  mediabuttons();
-  delay(500);
- 
-  delay(1000);*/
-  tft.drawRect(0,0,128,25,ST7735_WHITE);
-  tft.drawRect(0,25,128,80,ST7735_WHITE);
-  tft.drawRect(0,105,128,55,ST7735_WHITE);
-  tft.setTextColor(ST7735_WHITE);
-  tft.setCursor(5, 5);
-  tft.setTextSize(1);
-  tft.println("12:00");
-  tft.setTextSize(0);
-  tft.setCursor(5, 15);
-  tft.println("01.10.19");
-  tft.setCursor(105,5);
-  tft.print("90%");
-  tft.setTextSize(5);
-  tft.setCursor(20,50);
-  tft.print("19");
-  tft.setTextSize(1);
-  tft.println(" km/h");
-  tft.setCursor(5, 110);
-  tft.setTextSize(2);
-  tft.print("o 12'000");
-  tft.setTextSize(1);
-  tft.println(" rpm");
-  tft.setTextSize(2);
-  tft.setCursor(5, 140);
-  tft.print("H 90");
-  tft.setTextSize(1);
-  tft.println(" b/min");
-}
-/*Need time/date, pulse, S, S per trip, cadence, speed*/
-void loop() 
-{
-    
-}
-
-/*char* concat(char *s1, char *s2) 
-{
-    size_t len1 = strlen(s1);
-    size_t len2 = strlen(s2);                      
-    char *result = (char*)malloc(len1 + len2 + 1);
-    if (!result)
-        return NULL;
-    memcpy(result, s1, len1);
-    memcpy(result + len1, s2, len2 + 1);    
-    return result;
-}*/
